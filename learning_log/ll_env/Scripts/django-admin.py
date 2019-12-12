6ec770e8b85a9cf0b809b9309dad315965240a36
Stephen Joseph Samuels <stevesam1927@gmail.com>
2019-12-12 12:07:37 +0530

    Started PROJECT 3 - WEB APPLICATIONS - Viewing the Project

---
+++ learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\stevenjoseph19\web_applications\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

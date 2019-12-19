ea6e12fed7c70afdd498059503cfacaf7ccb1b29
Stephen Joseph Samuels <stevesam1927@gmail.com>
2019-12-19 19:40:31 +0530

    Using the Login Page

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -32,8 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # My apps
-    'learning_logs',
-
+        'learning_logs',
+        'users',
+        
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
